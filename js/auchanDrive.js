9ba677510f4f3ae5bac99cd3b902d03eca7ed437
roiKosmic <contact.vantoine@gmail.com>
2018-10-11 16:54:11 +0200

    Split drive in js files / Bug fix for carrefourdrive
    Warning firefox folder not updated with latest fix

---
+++ js/auchanDrive.js
@@ -0,0 +1,72 @@
+function addingExtensionToAuchanDrive(){
+	var img = chrome.extension.getURL("/img/openFood.png"); 
+
+	//Ajout dans la liste des produits sur le liste
+	$(".product-item__shortcuts").append("<img class='product-item__shortcutsButton  openfood' src='"+img+"'>");
+	
+	//Ajout dans le cas d'une liste
+	if ( $( ".operations-area" ).length ) {
+		$(".operations-area").append("<img class='openfood' src='"+img+"'>");
+		inListAuchan=true;
+	}
+}
+
+function addingObserverToAuchanDrive(){
+	if($( "main" ).length){
+	// The node to be monitored
+	 var target = $( "main" )[0];
+
+	// Create an observer instance
+	var observer = new MutationObserver(function( mutations ) {
+			mutations.forEach(function( mutation ) {
+				
+				var newNodes = mutation.addedNodes; // DOM NodeList
+				if( newNodes !== null ) { // If there are new nodes added
+						var $nodes = $( newNodes ); // jQuery set
+						var img = chrome.extension.getURL("/img/openFood.png"); 
+							$nodes.each(function() {
+								var $node = $( this );
+								
+								if( $node.hasClass( "product-item" ) ) {
+								
+									console.log("product inserted in DOM");
+									$node.find(".product-item__shortcuts").append("<img class='product-item__shortcutsButton  openfood' src='"+img+"'>");
+									
+								}
+							});
+				}
+			}); 
+			bindOpenFoodIconEvent();
+	});
+
+	// Configuration of the observer:
+	var config = { 
+		attributes: true, 
+		childList: true, 
+		characterData: true 
+	};
+ 
+	// Pass in the target node, as well as the observer options
+	observer.observe(target, config);
+	}
+}
+
+
+
+function getAuchanDriveQueryURL(elm){
+	var detail;
+	if(!inListAuchan){
+			detail = elm.closest("article").attr("data-name");
+	}else{
+		detail =elm.parent(".operations-area").prevAll(".libelle-produit").find("span").html();
+		console.log("inlist");
+	}
+	console.log(detail);
+	
+	//var preFilter = ['auchan bio','auchan'];
+	
+	return filterTitle(detail);
+
+
+}
+
---
+++ js/leclercDrive.js
@@ -0,0 +1,55 @@
+function addingExtensionToLeclerc(){
+	var img = chrome.extension.getURL("/img/openFood.png"); 
+	$(".divWCRS310_HD").append("<img class='openfood' src='"+img+"'>");
+	
+}
+
+function addingObserverToLeclercDrive(){
+
+// The node to be monitored
+	if($("#ulListeProduits").length){
+	 var target = $("#ulListeProduits")[0];
+
+	// Create an observer instance
+	var observer = new MutationObserver(function( mutations ) {
+			mutations.forEach(function( mutation ) {
+				
+				var newNodes = mutation.addedNodes; // DOM NodeList
+				if( newNodes !== null ) { // If there are new nodes added
+						var $nodes = $( newNodes ); // jQuery set
+						var img = chrome.extension.getURL("/img/openFood.png"); 
+							$nodes.each(function() {
+								var $node = $( this );
+								
+								if( $node.hasClass( "liWCRS310_Product" ) ) {
+									console.log("product inserted in DOM");
+									$node.find(".divWCRS310_HD").append("<img class='openfood' src='"+img+"'>");
+									
+								}
+							});
+				}
+			}); 
+			bindOpenFoodIconEvent();
+	});
+
+	// Configuration of the observer:
+	var config = { 
+		attributes: true, 
+		childList: true, 
+		characterData: true 
+	};
+ 
+	// Pass in the target node, as well as the observer options
+	observer.observe(target, config);
+	}
+}
+function getLeclercDriveQueryURL(elm){
+	var detail;
+
+	detail =elm.parent(".divWCRS310_HD").prevAll(".pWCRS310_Desc").find("a").html();
+	console.log("Detail leclerc :");
+	console.log(detail);
+	return filterTitle(detail);
+
+
+}
