bfebe24958094001b4354cc119af021a77d5ee97
mickaelFR <43605056+mickaelFR@users.noreply.github.com>
2020-05-25 14:23:08 +0200

    Update driveOff.class.js
    Problem : bindOpenFoodIconEvent() is called 2 times, by different instance of DriveOFF. One time in init() of DriveOFF.class.js and one time addingObserver in Drive.class.js (for MutationObserver).
    On some Drives, both are called, that make 2 binding on the same button, sending 2 times the search query to OFF. 
    The first attempt was to comment one of the two calls. Genérating malfunction on certains drive.
    Solution : adding a "global" var on content_script.js that can store each bind (can be use again later for other binds), then check if a bind function have been called, and if it's the case, return false (avoiding double binding).
    
    Remove the comment on line 131.
    Adding 3 lines on top of "bindOpenFoodIconEvent" :
    	if(DriveOffBinds['bindOpenFoodIconEvent'] === undefined){DriveOffBinds['bindOpenFoodIconEvent']=0;}
    	if(DriveOffBinds['bindOpenFoodIconEvent'] >= 1){return;}
    	++DriveOffBinds['bindOpenFoodIconEvent'];

--- js/driveOff.class.js
+++ js/driveOff.class.js
@@ -128,7 +128,7 @@ DriveOff.prototype = {
 		
 		this.bindEventToSnackbar();
 		// doublon avec localThis.DriveOffLocal.bindOpenFoodIconEvent();
-		//this.bindOpenFoodIconEvent();
+		this.bindOpenFoodIconEvent();
 	},
 	
 	buildSearchString:function(title){
@@ -176,6 +176,10 @@ DriveOff.prototype = {
 	/* ***** BINDING Functions ****** */
 	
 	bindOpenFoodIconEvent:function (){
+		if(DriveOffBinds['bindOpenFoodIconEvent'] === undefined){DriveOffBinds['bindOpenFoodIconEvent']=0;}
+		if(DriveOffBinds['bindOpenFoodIconEvent'] >= 1){return;}
+		++DriveOffBinds['bindOpenFoodIconEvent'];
+		
 		// SAVE "this" reference for .on local use to avoir conflict between 2 different "this" references
 		var DriveOffLocal = this;
 		
