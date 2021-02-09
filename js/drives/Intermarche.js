b9facb692851bcdefe81985e349bb12226b20684
Clovis Gauzy <github.788acf@clovisgauzy.fr>
2021-02-09 18:19:58 +0100

    Update Intermarché config

--- js/drives/Intermarche.js
+++ js/drives/Intermarche.js
@@ -2,30 +2,31 @@ drivesList.push(class Fr_Intermarche extends Drive {
   static driveName = 'Intermarché' 
   static domain = /intermarche\.com$/
   static lang = 'fr'
+  static country = 'france'
 
   static structure = {
     productView: {
-      base: '.ReactModal__Content',
+      base: '.ProductContainer__ProductContainerWrapper-sc-1lqtf3p-0',
       name: 'h1',
       mainDescription: '.styled__ProductGeneralInfoContainer-rc4bd7-0',
       description: '.ProductInformations__MainWrapper-sc-1vg3q1s-0',
-      ean: () => /product\/([0-9]{13})/i.exec(new URL(document.URL).pathname)[1]
+      ean: () => {
+        const reg = /product\/([0-9]{8,13})/i.exec(new URL(document.URL).pathname)
+        if (reg[1]) return Number(reg[1])
+      }
     },
     listView: {
       base: [
         '.styled__ProductGridWrapper-sc-15s6tne-0',
-        // '.slick-list',
-        '.carousel-produits__frame'
+        '.carousel-produits__wrapper'
       ],
       product: {
-        base: [
-          '.styled__ProductGridCell-sc-15s6tne-1',
-          // class="productTileV2Styled__TileWrapper-sc-19ad4vz-0 kYBHZZ"
-          // '.slick-slide'
-        ],
+        base: '.styled__ProductGridCell-sc-15s6tne-1',
         name: '.productTileV2Styled__DescriptionWrapper-sc-19ad4vz-9',
         mainDescription: '.textWrapperProductTile',
-        ean: base => base.dataset.id
+        ean: base => {
+          if (base.dataset.id) return Number(base.dataset.id)
+        }
       }
     }
   }
