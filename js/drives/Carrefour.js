71581063568af376369a3fc5d94e1889a2fd0ddc
Clovis Gauzy <github.788acf@clovisgauzy.fr>
2021-02-09 18:18:43 +0100

    Update Carrefour config

--- js/drives/Carrefour.js
+++ js/drives/Carrefour.js
@@ -2,6 +2,7 @@ drivesList.push(class Fr_Carrefour extends Drive {
   static driveName = 'Carrefour' 
   static domain = /carrefour\.fr$/
   static lang = 'fr'
+  static country = 'france'
 
   static structure = {
     productView: {
@@ -10,22 +11,23 @@ drivesList.push(class Fr_Carrefour extends Drive {
       mainDescription: '.main-details__right',
       // description: '.ProductInformations__MainWrapper-sc-1vg3q1s-0',
       ean: () => {
-        return Number(/p\/.*([0-9]{13})/i.exec(new URL(document.URL).pathname)[1])
+        return Number(/p\/.*([0-9]{8,13})/i.exec(new URL(document.URL).pathname)[1])
       }
     },
     listView: {
       base: [
-        '.product-list',
-        '.recommendations .ds-carousel__items'
-        
+        '.product-list ul',
+        '.editorial-product-list__products',
+        '.carousel--gutters',
+        '.recommendations'
       ],
       product: {
-        base: [
-          'article'
-        ],
+        base: 'article',
         name: 'h2',
         mainDescription: '&',
-        ean: base => Number(base.id)
+        ean: base => {
+          return Number(base.id)
+        }
       }
     }
   }
